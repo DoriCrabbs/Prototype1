60625fe1b71a22dc21bcd61e70b445cc7364fbae
Dori Crabbs <doricrabbs@gmail.com>
2020-06-20 22:47:48 +1000

    Finishing prototpye1 level

---
+++ Assets/Prototype1/Scripts/CageButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CageButton : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject winningText;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (winningText == null)
+        {
+            return;
+        }
+        winningText.SetActive(true);
+        Time.timeScale = 0f;
+    }
+}
